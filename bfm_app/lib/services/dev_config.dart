80b7454f34e17ae8799f4cf53fe989aa72983805
Luke <luke.fraserbrown04@gmail.com>
2026-03-11 09:13:47 +1300

    release v1.0.1

--- bfm_app/lib/services/dev_config.dart
+++ bfm_app/lib/services/dev_config.dart
@@ -2,4 +2,4 @@ import 'package:flutter/foundation.dart';
 
 /// Dev mode is only active in debug builds. In release/profile builds this is
 /// always false, which disables the dev menu, debug routes, and auth bypass.
-const bool kDevMode = true;//kDebugMode;
+const bool kDevMode = kDebugMode;
